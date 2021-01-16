c4ff100314087b70a7bac1cae50d8807b7ed1b17
zhouxiang <zhouxiang@twsz.com>
2021-01-17 00:53:04 +0800

    slot addTask

--- qt/qt5_mastering_demo01/demo/mainwindow.cpp
+++ qt/qt5_mastering_demo01/demo/mainwindow.cpp
@@ -6,9 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->addTaskButton, &QPushButton::clicked,
+        this, &MainWindow::addTask);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::addTask()
+{
+    qDebug() << "User clicked on the button!";
 }
